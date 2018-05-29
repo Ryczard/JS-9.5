04d673679daa5729df8b82cca3919362e3d6489c
Konrad Ryczek <konradryczek92@gmail.com>
2018-05-29 10:22:38 +0200

    Update script.js content

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var withButtonItemsClass = document.getElementsByClassName('button');
 
-for (var i = 0 ; i < withButtonItemsClass.length ; i++){
+for (var i = 0; i < withButtonItemsClass.length; i++){
 	alert(withButtonItemsClass[i].innerText);
 };
